6270d3f8e1bed59570376e4d518afc98a20416aa
piyanshugehani <piyanshugehani@gmail.com>
2024-09-01 12:43:06 +0530

    modified chart

--- client/urbansync/app/_components/LineChart.jsx
+++ client/urbansync/app/_components/LineChart.jsx
@@ -1,8 +1,27 @@
+// "use client"
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, PointElement } from 'chart.js';
+import { Line } from "react-chartjs-2";
 
-ChartJS.register(CategoryScale, LinearScale, LineElement, PointElement, Title, Tooltip, Legend);
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale, // x axis
+  LinearScale, // y axis
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Legend,
+  Tooltip,
+  Filler
+);
 
 const LineChart = () => {
   const data = {
@@ -11,16 +30,36 @@ const LineChart = () => {
       {
         label: 'Assigned to Me',
         data: [5, 9, 7, 8, 6],
-        borderColor: 'rgb(67, 24, 255)',
-        backgroundColor: 'rgb(67, 24, 255)',
+        borderColor: "rgb(67, 24, 255)",
+        borderWidth: 3,
+        pointBorderColor: "rgb(67, 24, 255)",
+        pointBorderWidth: 3,
+        // tension: 0.5,
         fill: true,
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx;
+          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, "rgba(67, 24, 255, 0.7)"); // Adjusted transparency
+        gradient.addColorStop(1, "rgba(67, 24, 255, 0)");
+          return gradient;
+        },
       },
       {
         label: 'Created by Me',
         data: [4, 6, 5, 10, 9],
         borderColor: 'rgb(106, 210, 255)',
-        backgroundColor: 'rgb(106, 210, 255)',
+        borderWidth: 3,
+        pointBorderColor: "rgb(106, 210, 255)",
+        pointBorderWidth: 3,
+        // tension: 0.5,
         fill: true,
+        backgroundColor: (context) => {
+          const ctx = context.chart.ctx;
+          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, "rgba(106, 210, 255, 0.7)"); // Adjusted transparency
+        gradient.addColorStop(1, "rgba(106, 210, 255, 0)");
+          return gradient;
+        },
       },
     ],
   };
